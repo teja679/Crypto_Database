e0697f7740f4e07fd5a2f2a5cc7bdff1e717750b
Teja <lovlytejaa@gamil.com>
2022-10-11 11:39:41 +0530

    images added

--- src/components/HomePageComponents/LandingPageComponents/index.js
+++ src/components/HomePageComponents/LandingPageComponents/index.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import './styles.css'
+import { motion } from 'framer-motion'
+import { RWebShare } from 'react-web-share'
+import iPhone from './../../assets/iphone.png'
+import gradient from './../../assets/gradient.png'
 
 const LandingPageComponent = () => {
   return (
     <div className='main-page'>
       <div className='title'>
-        <h2>Track Crypto <br />
-          <span>Real Time.</span>
-        </h2>
+        <motion.h1 className='big-heading' initial={{opacity: 0, y: 50}}
+        animate={{ opacity: 1, y:0}} transition={{ type: "spring", duration: 1}}>
+          <span className='stroke'>Track Crypto </span>
+          <br />
+          <span className='header-blue'>Real Time.</span>
+        </motion.h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         <div className='buttons'>
           <button className='btn btn1'>Dashboard</button>
+          <RWebShare data={{ text: 'Chacekout my crypto tracker made using React!',
+            url: "https://my-cryptotracker.com", title: "crypto Tracker"}}>
           <button className='btn btn2'>Share</button>
+          </RWebShare>
         </div>
+      </div>
+      <div className='img-box'>
+        <img className='gradient' src={gradient} alt='radient' />
+        <motion.img
+          src={iPhone.png} className='phone'
+          initial={{ y: -10 }}
+          animate={{ y: 10 }}
+          transition={{
+            type: 'smooth',
+            repeatType: 'mirror',
+            duration: '2',
+            repeat: 'infinity'
+          }}
+        />
       </div>
     </div>
   )
