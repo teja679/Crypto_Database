62646c29ba59a7ea5814e12710a500bda5f40684
Teja <lovlytejaa@gamil.com>
2022-10-11 19:51:27 +0530

    buttons and landingpage  component

--- src/components/HomePageComponents/LandingPageComponents/index.js
+++ src/components/HomePageComponents/LandingPageComponents/index.js
@@ -4,30 +4,40 @@ import { motion } from 'framer-motion'
 import { RWebShare } from 'react-web-share'
 import iPhone from './../../assets/iphone.png'
 import gradient from './../../assets/gradient.png'
+import Button from '../../Button'
+import { color } from '@mui/system'
+import OutlineButton from '../../OutlineButton'
 
 const LandingPageComponent = () => {
   return (
-    <div className='main-page'>
-      <div className='title'>
-        <motion.h1 className='big-heading' initial={{opacity: 0, y: 50}}
-        animate={{ opacity: 1, y:0}} transition={{ type: "spring", duration: 1}}>
+    <div className='flex-wrapper'>
+      <div className='heading-container'>
+        <motion.h1 className='big-heading' 
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }} 
+          transition={{ type: "spring", duration: 1 }}
+        >
           <span className='stroke'>Track Crypto </span>
           <br />
           <span className='header-blue'>Real Time.</span>
         </motion.h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         <div className='buttons'>
-          <button className='btn btn1'>Dashboard</button>
-          <RWebShare data={{ text: 'Chacekout my crypto tracker made using React!',
-            url: "https://my-cryptotracker.com", title: "crypto Tracker"}}>
-          <button className='btn btn2'>Share</button>
+          <a href='/dashboard'>
+            <Button text={'Dashboard'} />
+          </a>
+          <RWebShare data={{
+            text: 'Chacekout my crypto tracker made using React!',
+            url: "https://my-cryptotracker.com", title: "crypto Tracker"
+          }}>
+            <OutlineButton text={'share'} />
           </RWebShare>
         </div>
       </div>
       <div className='img-box'>
-        <img className='gradient' src={gradient} alt='radient' />
+        <img className='gradient' src={gradient} alt='gradient' />
         <motion.img
-          src={iPhone.png} className='phone'
+          src={iPhone} className='phone'
           initial={{ y: -10 }}
           animate={{ y: 10 }}
           transition={{
